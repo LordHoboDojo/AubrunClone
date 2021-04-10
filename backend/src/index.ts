fb3ce035b07e17e1f8ad6b37a6f061a13832febc
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2021-04-10 14:26:23 -0500

    General Commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 var bodyParser = require('body-parser')
-cosnt app =express();
-onst app = express();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../../frontend/build')))
-connect();
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+
+app.listen(3100, () => {
+    console.log('Starting API on port 3100...');
+});
