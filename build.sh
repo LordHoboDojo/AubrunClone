bd412d477cfd2c8f565752851997036865e04027
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2021-04-11 10:23:10 +0000

    Cloud Update

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 echo "Building project..."
 echo "Building frontend..."
 cd ./frontend/
+npm install
+npm install axios
 npm run build
 cd ..
 echo "Building backend..."
 cd ./backend/
+npm install
+npm install express
+npm install axios
 npm run build
 cd ..
 echo "Build complete..."
